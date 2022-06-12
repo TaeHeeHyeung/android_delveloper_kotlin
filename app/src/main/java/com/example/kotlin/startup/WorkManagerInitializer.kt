e7e72944f77f050bf0585259e3e0a6382006a83a
TaeHee_Hyeung <hth0893@naver.com>
2022-06-12 16:05:57 +0900

    generic 공부 out (kotlin),  extends(java) /  in (kotlin), super(java)

--- app/src/main/java/com/example/kotlin/startup/WorkManagerInitializer.kt
+++ app/src/main/java/com/example/kotlin/startup/WorkManagerInitializer.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.startup.Initializer
 import androidx.work.Configuration
 import androidx.work.WorkManager
+import java.util.*
+import kotlin.collections.ArrayList
 
 //Startup library =>  컴포넌트를 초기화 할 때 사용한다.
 //이전에 앱에서 컴포넌트 초기화를 위해 content provider 를 사용한 경우, App Startup 사용 시에는 제거해야 한다.
@@ -15,7 +17,22 @@ class WorkManagerInitializer : Initializer<WorkManager> {
         return WorkManager.getInstance(context)
     }
 
+    //java
+    //generic
+    // <? extends Foo>: ? 에는 Foo 을 상속한 어떤 것도 사용가능 (사용 확장) ex) (class name: Foo 사용가능)
+    // <? super Integer>: ?에는 Integer 의 상위 타입만 사용 가능 (사용 제한) ex) (Object Number, Integer 사용가능)
+
+    //kotlin => java
+    // out: extends
+    // in: super
+
+
+    //in, out
+
     override fun dependencies(): MutableList<Class<out Initializer<*>>> {
+
+//        var list: ArrayList<*>
+//        list.add("string")
         return mutableListOf()
     }
 
