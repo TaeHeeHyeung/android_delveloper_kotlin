3be5c11bcdf771007aaceadb11814e31bb679869
TaeHee_Hyeung <hth0893@naver.com>
2022-06-12 17:03:50 +0900

    Needs  Android 스튜디오 Arctic Fox  LiveData, ViewModel, 뷰 결합 등 아키텍처 구성요소와 앱 아키텍처 가이드에서 권장하는 아키텍처에 관한 기본 지식  코루틴 및 Kotlin Flow 에 관한 기본 지식  example: https://developer.android.com/codelabs/android-preferences-datastore?hl=ko#0

---
+++ app/src/main/java/com/example/kotlin/datastore/PreferencesDatastore.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin.datastore
+
+
+// example: https://developer.android.com/codelabs/android-preferences-datastore?hl=ko#0
+
+// Needs
+// Android 스튜디오 Arctic Fox
+// LiveData, ViewModel, 뷰 결합 등 아키텍처 구성요소와 앱 아키텍처 가이드에서 권장하는 아키텍처에 관한 기본 지식
+// 코루틴 및 Kotlin Flow 에 관한 기본 지식
+object PreferencesDatastore  {
+}
